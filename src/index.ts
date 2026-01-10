2a3befe4c3bf5343bcbd27e017e95d41f73714fd
Sumuditha <sumuditha356@gmail.com>
2026-01-10 12:07:31 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(
     cors({
-        origin: ["http://localhost:5173", "https://cinetime-tracker.vercel.app/"],
+        origin: ["https://cinetime-tracker.vercel.app/" ,"http://localhost:5173"],
         methods: ["GET", "POST", "PUT", "DELETE"]
     })
 );
